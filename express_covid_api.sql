274f8d358fe2286e54380b54f8c6ed142ddf3132
sabrina-smrna <marselasela3445@gmail.com>
2025-01-15 15:15:33 +0700

    upload database

---
+++ express_covid_api.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Jan 2025 pada 09.14
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `express_covid_api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `patients`
+--
+
+CREATE TABLE `patients` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `status` enum('positive','recovered','dead') DEFAULT NULL,
+  `in_date_at` date DEFAULT NULL,
+  `out_date_at` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `patients`
+--
+
+INSERT INTO `patients` (`id`, `name`, `phone`, `address`, `status`, `in_date_at`, `out_date_at`) VALUES
+(1, 'Rinjani', '081234567890', 'Jl. Merdeka No. 1, Jakarta', 'positive', '2025-01-15', NULL),
+(2, 'Jesi', '081234967891', 'Jl. Merdeka No. 2, Jakarta', 'recovered', '2025-01-15', '2025-01-15'),
+(3, 'Tomi', '081233567892', 'Jl. Kesempurnaan No. 3, Jakarta', 'dead', '2025-01-15', '2025-01-12');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
